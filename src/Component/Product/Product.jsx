4c14971ab0baef4590c42a038b06a633c0d1ee16
Munshi Muntasir Rifat <www.muntasirrifat23@gmail.com>
2023-03-31 02:13:52 +0600

    fake data

---
+++ src/Component/Product/Product.jsx
@@ -0,0 +1,21 @@
+import React, { useEffect, useState } from 'react';
+
+const Product = () => {
+    const[product, setProduct]= useState([]);
+    useEffect(()=>{
+        fetch('')
+    }, [])
+    return (
+        <div>
+            <div>
+
+            </div>
+
+            <div>
+
+            </div>
+        </div>
+    );
+};
+
+export default Product;
