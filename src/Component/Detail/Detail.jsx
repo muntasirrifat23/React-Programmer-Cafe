1efe360c926495fc2491f26ff8efc72bbded8ebe
Munshi Muntasir Rifat <www.muntasirrifat23@gmail.com>
2023-03-31 16:53:19 +0600

    title & mark as

--- src/Component/Detail/Detail.jsx
+++ src/Component/Detail/Detail.jsx
@@ -1,25 +1,36 @@
 import React from 'react';
 import './Detail.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
 const Detail = (props) => {
     console.log(props)
-    const{img, name,profile, date,read}= props.product;
+    const{img, name,profile, date,read,title}= props.product;
     return (
         <div className='data'>
+            <div className='position'>
             <img src={img} alt="" />
             
             <div className='middle'>
             <div className='ndate'>
                 <p className='profile'><img src={profile} alt="" /></p>
-                <div className='r'>
+              <div className='r'>
                 <h5 className='name'>{name}</h5> 
                 <h6 className='date'>{date}</h6>
+              </div>
+            </div>
+            <div>
+                <h6 className='read'>{read} read</h6>
+                <button>iohuhjjo</button>
+            </div>   
+            </div>
+            {/* Title */}
+            <div>
+            <h3  className='title'>{title}</h3>
+            </div>
+            <button className='mark'>Mark as read</button>
             </div>
             </div>
-                <h6>{read}</h6>
-                <button>ioo</button>
-            </div>
-           
-        </div>
+        
     );
 };
 
