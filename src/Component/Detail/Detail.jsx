d74be91355e88e5e83531eb1e612b0f59b730ca8
Munshi Muntasir Rifat <www.muntasirrifat23@gmail.com>
2023-03-31 17:04:31 +0600

    description

--- src/Component/Detail/Detail.jsx
+++ src/Component/Detail/Detail.jsx
@@ -4,7 +4,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 const Detail = (props) => {
     console.log(props)
-    const{img, name,profile, date,read,title}= props.product;
+    const{img, name,profile, date,read,title,description}= props.product;
     return (
         <div className='data'>
             <div className='position'>
@@ -27,6 +27,7 @@ const Detail = (props) => {
             <div>
             <h3  className='title'>{title}</h3>
             </div>
+            <p>{description}</p>
             <button className='mark'>Mark as read</button>
             </div>
             </div>
